29900076bb37d2b7aa88e3682ae83baa690f027c
Sagitova Adelia <adeliya64775@gmail.com>
2025-01-02 19:30:41 +0500

    Добавлены все страницы, настроена базовая маршрутизация

--- djangoapp/main/urls.py
+++ djangoapp/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name="index"),
+    path('statisctics/', views.get_statistics, name='statistics'),
+    path('demand/', views.get_demand, name='demand'),
+    path('geography/', views.get_geography, name='geography'),
+    path('skills/', views.get_skills, name='skills'),
+    path('last_vacancies/', views.get_last_vacancies, name='last_vacancies'),
 ]
--- djangoapp/main/views.py
+++ djangoapp/main/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/index.html')
+
+def get_statistics(request):
+    return render(request, 'main/statistics.html')
+
+def get_demand(request):
+    return render(request, 'main/demand.html')
+
+def get_geography(request):
+    return render(request, 'main/geography.html')
+
+def get_skills(request):
+    return render(request, 'main/skills.html')
+
+def get_last_vacancies(request):
+    return render(request, 'main/last_vacancies.html')
