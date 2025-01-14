471b0c36f148b30bb28c5f1e5a1eda9c7f9651db
Sagitova Adelia <adeliya64775@gmail.com>
2025-01-14 22:41:55 +0500

    Выполнена вся статистика проекта

--- djangoapp/main/views.py
+++ djangoapp/main/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+import requests
 from django.shortcuts import render
 
 
@@ -17,4 +19,28 @@ def get_skills(request):
     return render(request, 'main/skills.html')
 
 def get_last_vacancies(request):
-    return render(request, 'main/last_vacancies.html')
+    now = datetime.now()
+
+    time = now - timedelta(hours=24)
+
+    str_time = time.strftime('%Y-%m-%dT%H:%M:%S')
+
+    response = requests.get('https://api.hh.ru/vacancies', params={
+        'text': 'System Administrator',
+        'date_from': str_time,
+        'per_page': 10,
+        'order_by': 'publication_time'
+    })
+
+
+    vacancies = response.json()['items']
+
+    for vacancy in vacancies:
+        vacancy_id = vacancy['id']
+        vacancy_response = requests.get(f'https://api.hh.ru/vacancies/{vacancy_id}')
+        key_skills = vacancy_response.json().get('key_skills', [])
+        vacancy['key_skills'] = ', '.join([skill['name'] for skill in key_skills])
+        description = vacancy_response.json().get('description')
+        vacancy['description'] = description
+
+    return render(request, 'main/last_vacancies.html', {'vacancies': vacancies})
